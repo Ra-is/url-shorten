4ee94a5e4b87790eaf0123b2c93cfa722a72ec48
Ra-is <osumanurais@gmail.com>
2022-12-12 23:39:24 +0000

    added button click

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -4,9 +4,15 @@
             
             <span class="text-white font-sans pb-2">Paste the URL to be shortened</span>
         <div class="flex flex-row  w-full p-3 px-10">
-            <input type="text" class="bg-white border flex-1 focus:border-none focus:border-black">
-            <button class="bg-blue-500 text-white p-2">Shorten</button>
+            <input type="text" class="bg-white border flex-1 focus:border-none focus:border-black" v-model="user_url">
+            <button class="bg-blue-500 text-white p-2" @click="shortenUrl">Shorten</button>
         </div>
+
+         <div class="flex flex-row items-center justify-center space-x-3">
+            <input class="text-white bg-black text-center font-sans p-2 focus:outline-none" value="test.url.com" ref="message"  readonly/>
+            <button class="bg-green-500 text-white p-1 px-4 rounded-md" @click="copyToClipboard" >Copy</button>
+         </div>
+        
           
         
         </div>
@@ -16,6 +22,23 @@
 
 <script>
 export default {
-
+    data(){
+        return {
+            user_url: null,
+        }
+    },
+methods:{
+    copyToClipboard(){
+        const element = this.$refs.message;
+      element.select();
+      element.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+    },
+    shortenUrl()
+    {
+        alert(this.user_url)
+        this.user_url = null
+    }
+}
 }
 </script>
